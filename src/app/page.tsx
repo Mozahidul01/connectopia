c34544fb84d2acd86d2d733e73d56cfb65dbd91a
mozahidul01 <mozo.postbox@gmail.com>
2023-07-05 22:40:44 +0600

    slight optimization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import GeneralFeed from "@/components/homepage/GeneralFeed";
 import MiniCreateCommunity from "@/components/homepage/MiniCreateCommunity";
 import { getAuthSession } from "@/lib/auth";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const session = await getAuthSession();
 
